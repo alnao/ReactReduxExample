bca2a80a238f8cace5469fb141005a0b669d83b6
Alberto Nao <alnao@alnao.it>
2021-10-31 12:57:42 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+const el=React.createElement('h1',{className:'benvenuto'},'My placess');
+ReactDOM.render(el,document.getElementById('root'));
+
+
+/*
+//originali 
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -15,3 +23,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
